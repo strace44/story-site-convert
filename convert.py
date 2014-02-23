84b0d620120ab3bd4c5358fb9f137b87658d11fd
strace44 <strace44@yahoo.com>
2014-02-22 23:11:25 -0500

    Manually replace unknown sequence too

--- convert.py
+++ convert.py
@@ -129,10 +129,11 @@ def tolerant_decode(story_data):
     fixed_quotes_1 = raw_decoded.replace('\udce2\udc80?', '\u201d')
     fixed_quotes_2 = fixed_quotes_1.replace('\udce2\udc80', '\u201d')
     fixed_letter_a_grave = fixed_quotes_2.replace('\udcc3', '\xe0')
+    fixed_unknown_sequence = fixed_letter_a_grave.replace('\udcef\udcbc', '\ufffd')
     # No other special cases: replace all surrogate escape chars with
     # their cp1252 interpretation
     surrogate_decoded = SURROGATE_ESCAPES.sub(surrogate_cp1252_replace,
-        fixed_letter_a_grave)
+        fixed_unknown_sequence)
     return surrogate_decoded
 
 def get_comment(hr_node):
